ff84b42b2a0894179aa49541d678b2f02225652e
Luis Veliz <luisvelizjr@gmail.com>
2019-01-10 12:16:19 -0700

    ignoring *.xuserState

---
+++ GlobalImpactApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GlobalImpactApp
+//
+//  Created by Veliz, Luis on 1/10/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
