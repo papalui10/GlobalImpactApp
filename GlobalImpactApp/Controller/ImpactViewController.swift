e4d585a34b37a0494a31136c800c4dc6f37ea790
Luis Veliz <luisvelizjr@gmail.com>
2019-01-10 12:51:19 -0700

    Added an extension for the zoom, alos IBoutlets for mapkit, button, image, and the label. Added the action func for the button as well.

--- GlobalImpactApp/Controller/ImpactViewController.swift
+++ GlobalImpactApp/Controller/ImpactViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+public class ImpactViewController: UIViewController
+{
+    @IBOutlet weak var MapKit: MKMapView!
+    @IBOutlet weak var Button: UIButton!
+    @IBOutlet weak var Image: UIImageView!
+    @IBOutlet weak var Label: UILabel!
+    
+   public override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func Button(_ sender: UIButton)
+    {
+        let impactLocation = CLLocationCoordinate2D(latitude: -17.650920, longitude: -149.426042)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 10.05, longitudeDelta: 10.05)
+        let impactRegion = MKCoordinateRegion(center: impactLocation, span: mapSpan)
+        MapKit.animatedZoom(zoomRegion: impactRegion, duration: 2.5)
+    }
+    
 
 }
 
+extension MKMapView
+{
+    public func animatedZoom(zoomRegion:MKCoordinateRegion, duration:TimeInterval)
+    {
+        MKMapView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 10, options: UIView.AnimationOptions.curveEaseIn, animations:
+            { self.setRegion(zoomRegion, animated: true) }, completion: nil)
+    }
+}
